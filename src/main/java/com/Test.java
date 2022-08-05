b6f4326d7af740996667ae07422bf534adf213b4
AN <huhanqi2004@gmail.com>
2022-08-05 21:08:39 +0800

    master-java2

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,7 +1,9 @@
 package com;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("java1");
+        System.out.println("java2");
     }
 }
